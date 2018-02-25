de9aeb66e33ee0b56b7d8a79281e0747f82ddf44
Qexy <admin@qexy.org>
2018-02-25 18:00:05 +0400

    File code refactoring & add new functions

--- Alonity/Alonity.php
+++ Alonity/Alonity.php
@@ -24,7 +24,7 @@ use Alonity\Triggers\Triggers as Triggers;
 class Alonity {
 
 	// Версия ядра
-	const VERSION = '0.3.9';
+	const VERSION = '0.4.0';
 
 	// Объект загруженного приложения
 	private $App = null;
--- Components/File/File.php
+++ Components/File/File.php
@@ -8,21 +8,22 @@
  *
  * @license https://www.gnu.org/licenses/gpl-3.0.html
  *
- * @version 1.1.0
+ * @version 2.0.0
  */
 
 namespace Alonity\Components;
+
+use Alonity\Components\File\Config;
+use Alonity\Components\File\Image;
+use Alonity\Components\File\Upload;
 
 use FileException;
 
 require_once(__DIR__.'/FileException.php');
 
 class File {
-	private static $cache = [];
 
 	private static $root = null;
-
-	private static $complection = ['name', 'type', 'error', 'tmp_name', 'size'];
 
 	public static function setRoot($dir){
 		self::$root = $dir;
@@ -36,63 +37,16 @@ class File {
 		return self::$root;
 	}
 
-	/**
-	 * Получает значения конфигурационного файла и локально кэширует полученный результат
-	 *
-	 * @param $name string
-	 *
-	 * @throws FileException
-	 *
-	 * @return mixed
-	*/
-	public static function getConfig($name){
-		$key = md5($name);
-
-		if(isset(self::$cache[$key])){
-			return self::$cache;
-		}
-
-		$filename = self::getRoot().$name;
-
-		if(!file_exists($filename) || !is_file($filename)){
-			throw new FileException("File not found in $filename");
-		}
-
-		self::$cache[$key] = (include($filename));
-
-		return self::$cache[$key];
+	public static function config(){
+		return new Config();
 	}
 
-	/**
-	 * Устанавливает значение в файл конфигурации. Если файл и/или диреткория не были найдены, то они будут созданы
-	 *
-	 * @param $name string
-	 * @param $data mixed
-	 *
-	 * @return mixed
-	*/
-	public static function setConfig($name, $data){
-		$key = md5($name);
+	public static function image(){
+		return new Image();
+	}
 
-		$filename = self::getRoot().$name;
-
-		$dir = dirname($filename);
-
-		if(!file_exists($dir)){
-			@mkdir($dir, 0755, true);
-		}
-
-		$date = date("d.m.Y H:i:s");
-
-		$result = var_export($data, true);
-
-		$data = "<?php // Updated: $date".PHP_EOL.PHP_EOL."return $result;".PHP_EOL.PHP_EOL."?>";
-
-		@file_put_contents($filename, $data);
-
-		self::$cache[$key] = $result;
-
-		return $result;
+	public static function upload(){
+		return new Upload();
 	}
 
 	/**
@@ -137,8 +91,6 @@ class File {
 				throw new FileException("file $files not removed");
 			}
 		}
-
-
 
 		return true;
 	}
@@ -208,138 +160,6 @@ class File {
 		}
 
 		return true;
-	}
-
-	private static function restructFiles($files){
-		if(!is_array($files) || empty($files)){
-			throw new FileException("param files must be array");
-		}
-
-		$result = [];
-
-		foreach(self::$complection as $v){
-			if(!isset($files[$v])){
-				throw new FileException("file is not complected");
-				break;
-			}
-
-			if(empty($files[$v])){
-				throw new FileException("file must be not empty");
-				break;
-			}
-
-			if(is_array($files[$v])){
-				foreach($files[$v] as $k => $val){
-					$result[$k][$v] = $val;
-				}
-			}else{
-				$result[0][$v] = $val;
-			}
-		}
-
-		return $result;
-	}
-
-	/**
-	 * Загружает файлы на сервер. Для загрузки используется формат $_FILES
-	 * Возвращает массив полных путей до загруженных файлов
-	 *
-	 * @param $files array
-	 * @param $options array
-	 *
-	 * @throws FileException
-	 *
-	 * @return array
-	*/
-	public static function upload($files, $options=[]){
-
-		if(!is_array($options)){
-			throw new FileException('param options must be array');
-		}
-
-		if(!is_array($files)){
-			throw new FileException("param files must be array");
-		}
-
-		if(!isset($options['extensions'])){
-			$options['extensions'] = [];
-		}
-
-		if(!isset($options['maxsize'])){
-			$options['maxsize'] = 0;
-		}
-
-		if(!isset($options['rename'])){
-			$options['rename'] = false;
-		}
-
-		if(!isset($options['dir'])){
-			$options['dir'] = '/Uploads/files';
-		}
-
-		if(!isset($options['maxfiles'])){
-			$options['maxfiles'] = 0;
-		}
-
-		if(!file_exists($options['dir'])){
-			@mkdir($options['dir'], 0755, true);
-		}
-
-		$options['dir'] = self::getRoot().$options['dir'];
-
-		$files = self::restructFiles($files);
-
-		$result = [];
-
-		$num = 0;
-
-		foreach($files as $k => $file){
-
-			$num++;
-
-			if($options['maxsize']>0 && $file['size']>$options['maxsize']){
-				throw new FileException("max file size is {$options['maxsize']}b");
-			}
-
-			if(!empty($file['error'])){
-				throw new FileException($file['error']);
-			}
-
-			$newname = $file['name'];
-
-			$info = pathinfo($file['name']);
-
-			if(is_string($options['rename'])){
-				$newname = $options['rename'];
-			}elseif($options['rename']===true){
-				$newname = md5(mt_rand(0,9999999).mt_rand(0,9999999));
-			}
-
-			if(!empty($options['extensions'])){
-				if(!in_array($options['extensions'], $info['extension'])){
-					throw new FileException('upload only for files '.implode(', ', $options['extensions']));
-				}
-			}
-
-			@move_uploaded_file($file['tmp_name'], $options['dir'].'/'.$newname);
-
-			$result[] = "{$options['dir']}/$newname";
-
-			if($options['maxfiles']>0 && $options['maxfiles']>=$num){
-				break;
-			}
-		}
-
-		return $result;
-	}
-
-	/**
-	 * Очищает кэш
-	 *
-	 * @return void
-	*/
-	public static function clearCache(){
-		self::$cache = [];
 	}
 }
 
