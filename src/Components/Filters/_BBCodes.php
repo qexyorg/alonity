98c576465243f173020b81bcf67cefba67450bec
Qexy <admin@qexy.org>
2019-06-03 02:29:16 +0300

    Change bbcode tags & add new methods

--- src/Components/Filters/_BBCodes.php
+++ src/Components/Filters/_BBCodes.php
@@ -3,17 +3,19 @@
  * BBCodes filter component of Alonity Framework
  *
  * @author Qexy <admin@qexy.org>
- * @copyright Copyright (c) 2018, Qexy
+ * @copyright Copyright (c) 2019, Qexy
  * @link http://qexy.org
  *
  * @license https://www.gnu.org/licenses/gpl-3.0.html
  *
- * @version 2.0.0
+ * @version 2.1.0
  */
 
 namespace Framework\Components\Filters;
 
 class _BBCodes {
+
+	private static $patterns = null;
 
 	private static function preg_replace_recursive($pattern, $replacement, $string){
 
@@ -23,8 +25,14 @@ class _BBCodes {
 	}
 
 	private static function codeparse($string){
-		return preg_replace_callback("/\[code\](.*)\[\/code\]/iUs", function($matches){
-			return '<code>'.str_replace(['[', ']'], ['&#91;', '&#93;'], $matches[1]).'</code>';
+		return preg_replace_callback("/\[code(=\"(\w+)\")?\](.*)\[\/code\]/isU", function($matches){
+			$str = str_replace(['[', ']'], ['&#91;', '&#93;'], $matches[3]);
+
+			if(!empty($matches[2])){
+				return '<div class="bb-code"><div class="bb-code-language">'.$matches[2].'</div><div class="bb-code-text">'.$str.'</div></div>';
+			}else{
+				return '<div class="bb-code">'.$str.'</div>';
+			}
 		}, $string);
 	}
 
@@ -49,8 +57,20 @@ class _BBCodes {
 		return nl2br($string);
 	}
 
+	public static function addPattern($regexp, $replacement){
+		self::$patterns[$regexp] = $replacement;
+	}
+
+	public static function delPattern($regexp){
+		if(isset(self::$patterns[$regexp])){
+			unset(self::$patterns[$regexp]);
+		}
+	}
+
 	private static function getPatterns(){
-		return [
+		if(!is_null(self::$patterns)){ return self::$patterns; }
+
+		self::$patterns = [
 			'\[b\](.*)\[\/b\]' => '<b class="bb-bold">$1</b>',
 
 			'\[i\](.*)\[\/i\]' => '<i class="bb-italic">$1</i>',
@@ -67,7 +87,7 @@ class _BBCodes {
 
 			'\[line\]' => ['<div class="bb-line"></div>', false],
 
-			'\[youtube\]https\:\/\/www\.youtube\.com\/watch\?v=([\w\-]{5,15})\[\/youtube\]' => ['<iframe width="516" height="290" src="https://www.youtube.com/embed/$1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>', false],
+			'\[youtube\]https\:\/\/www\.youtube\.com\/watch\?v=([\w\-]{5,15})\[\/youtube\]' => ['<iframe class="bb-youtube" width="516" height="290" src="https://www.youtube.com/embed/$1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>', false],
 
 			'\[spoiler\](.*)\[\/spoiler\]' => '<div class="bb-spoiler-wrapper"><div class="bb-spoiler"><a href="#" class="bb-spoiler-trigger">Спойлер</a><div class="bb-spoiler-text">$1</div></div></div>',
 
@@ -87,6 +107,8 @@ class _BBCodes {
 
 			'\[url\]((?:f|ht)(?:tp)s?\:\/\/[^\s"]+)\[\/url\]' => ['<a class="bb-url" href="$1">$1</a>', false],
 		];
+
+		return self::$patterns;
 	}
 }
 
